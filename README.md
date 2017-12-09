97024df41cda00a0169b90d4b6b0ff67e80f7e06
Atul R <atulanand94@gmail.com>
2017-12-09 17:24:20 +0530

    Added plugin

---
+++ README.md
@@ -0,0 +1,64 @@
+# gitbook-plugin-expo-snack
+
+A gitbook plugin to include expo snack to your documentations/books
+
+### Instructions
+
+In your `book.json`
+
+```
+{
+  "plugins": ["expo-snack"]
+}
+```
+
+### Usage:
+To use the snack viewer
+1. Generate a snack url in expo.io
+2. Then use it as follows
+
+```
+{% exposnack %}
+snack url
+{% endexposnack %}
+```
+
+**For example:**
+
+```
+{% exposnack %}
+@master-atul/test
+{% endexposnack %}
+```
+
+### Screenshot
+
+<br>
+
+<div style="text-align:center">
+  <img src="https://github.com/master-atul/gitbook-plugin-expo-snack/raw/master/screens/preview.png" style="width: 100%;display: inline;">
+</div>
+<br>
+
+
+
+#### Advanced:
+
+```
+{% exposnack
+  platform="ios"
+  preview="true"
+  theme="dark"
+  style="overflow:hidden;background:#212733;border:1px solid rgba(0,0,0,.16);border-radius:4px;height:505px;width:100%
+%}
+@master-atul/test
+{% endexposnack %}
+```
+*The above mentioned example is the default*
+
+#### Options:
+
+- platform=`"android" | "ios"`
+- preview=`"true|false"`
+- theme="`light|dark`"
+- style=`"overflow:hidden;background:#212733;"`
--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-module.exports = {
-  // Map of hooks
-  hooks: {},
+/*
 
-  // Map of new blocks
+{% exposnack platform="android|ios" preview="true|false" theme="light|dark" style="overflow:hidden;background:#212733;" %}
+@master-atul/test
+{% endexposnack %}
+
+*/
+
+module.exports = {
   blocks: {
     exposnack: {
-      process: function(block) {
-        return "Hello , How are you?";
+      process: function process(block) {
+        var snackId = String(block.body).trim() || "@master-atul/test";
+        var kwargs = block.kwargs || {};
+        var platform = kwargs.platform || "ios";
+        var preview = kwargs.preview || true;
+        var theme = kwargs.theme || "dark";
+        var style =
+          kwargs.style ||
+          "overflow:hidden;background:#212733;border:1px solid rgba(0,0,0,.16);border-radius:4px;height:505px;width:100%";
+        return (
+          '<div data-snack-id="' +
+          snackId +
+          '" data-snack-platform="' +
+          platform +
+          '" data-snack-preview="' +
+          preview +
+          '" data-snack-theme="' +
+          theme +
+          '" style="' +
+          style +
+          '"></div><script async src="https://snack.expo.io/embed.js"></script>'
+        );
       }
     }
-  },
-  // Map of new filters
-  filters: {}
+  }
 };
